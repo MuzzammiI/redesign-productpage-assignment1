91e6bbab4bf399dd292ec2faa6c7fe0ee7e02495
muzzu <www.mdmozammil@gmail.com>
2025-04-28 21:00:03 +0530

    install firebase sdk

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     }
   },
   build: {
-    // outDir: 'build', // default is 'dist'
-    outDir:'dist'
+    outDir: 'build', // default is 'dist'
+    // outDir:'dist'
   }
 })
